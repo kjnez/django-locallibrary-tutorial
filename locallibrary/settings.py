9b94004099e967d4b86697139724547c525991f1
Yingnan Cui <yingn.cui@gmail.com>
2023-05-20 17:50:25 -0400

    Changing it back.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # Set hosts to allow any app on Railway and the local testing URL
-ALLOWED_HOSTS = ['*.railway.app','127.0.0.1']
+ALLOWED_HOSTS = ['locallib-6d13.up.railway.app','127.0.0.1']
 
 # Set CSRF trusted origins to allow any app on Railway and the local testing URL
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://locallib-6d13.up.railway.app']
 
 
 # Application definition
